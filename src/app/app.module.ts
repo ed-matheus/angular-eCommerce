163db66d9497e4f86c5f5c65e976c139bd5e877d
ed-matheus <edsonmatheus02@hotmail.com>
2023-06-08 20:50:22 -0300

    criado componente da página 'home' e alterada a rota principal para o mesmo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ProductCardComponent } from './components/product-card/product-card.com
     HeaderComponent,
     BannerComponent,
     ProductCardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
