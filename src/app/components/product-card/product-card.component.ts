5ee92f412313a27490dd9a1d1da15e90083c816b
ed-matheus <edsonmatheus02@hotmail.com>
2023-06-09 20:22:02 -0300

    pagina 'home' finalizada

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
+
+  @Input()
+  image:string = '';
+
+  @Input()
+  name:string = '';
+
+  @Input()
+  price:string = '';
+
+  @Input()
+  description:string = '';
 
   constructor() { }
 
